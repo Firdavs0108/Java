03c7a8861cf8c8f2e6d2e3f2b74bd0c91ebb9869
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-26 09:21:54 +0900

    java

---
+++ class_1/Array.java
@@ -0,0 +1,16 @@
+package class_1;
+
+import java.util.Arrays;
+
+public class Array {
+public static void main(String[] args) {
+    
+    String[] names = {"Ali", "Firdavs", "Abu"};
+
+    names[2] = "Abubek";
+
+    System.out.println(Arrays.toString(names));
+    System.out.println(names[2]);
+}
+   
+}
